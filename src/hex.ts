1414633defd90c64a558a8e075cd910a9b188a73
Dead End <dead.end@inbox.ru>
2024-12-31 11:34:28 +0100

    add hexGetCricle

--- src/hex.ts
+++ src/hex.ts
@@ -34,3 +34,21 @@ export const hexGetCenter = (
     y: center.y + hex.y * size * SQR_3 + (hex.x * size * SQR_3) / 2,
   };
 };
+
+export const hexGetCricle = (radius: number) => {
+  const results: Point[] = [];
+
+  let hex: Point = {
+    x: -1 * radius,
+    y: radius,
+  };
+
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < radius; j++) {
+      results.push(hex);
+      hex = hexGetNeighbor(hex, i);
+    }
+  }
+
+  return results;
+};
