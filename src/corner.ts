1dda4fd67bd7b51cd4a6e1762ff16a9cf782a896
Dead End <dead.end@inbox.ru>
2025-01-01 12:30:13 +0100

    internal init

--- src/corner.ts
+++ src/corner.ts
@@ -1,25 +1,27 @@
 import { Point } from './types';
 import { mod } from './utils';
 
-const corners: Point[] = [];
-
 /**
  * 0: -120 degree (top left)
  * 1: -60 degree (top right)
  * 2: 0 degree (right)
  * ...
  */
-export const cornerInit = () => {
+const cornerInit = () => {
+  const result: Point[] = [];
   for (let i = -2; i < 4; i++) {
     const degree = 60 * i;
     const rad = (Math.PI / 180) * degree;
 
-    corners.push({
+    result.push({
       x: Math.cos(rad),
       y: Math.sin(rad),
     });
   }
+  return result;
 };
+
+const corners: Point[] = cornerInit();
 
 export const cornerGet = (center: Point, i: number, size: number): Point => {
   const idx = mod(i, 6);
