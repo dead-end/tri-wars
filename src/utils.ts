eae5ec41c0ed9b211958e4f42498942fa0aeec2c
Dead End <dead.end@inbox.ru>
2025-01-05 13:08:56 +0100

    removed sqrt

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,3 @@
  * Compute a modulo value that is positive.
  */
 export const mod = (n: number, m: number) => ((n % m) + m) % m;
-
-/**
- * The value is used for some hexagon computations.
- */
-export const SQR_3 = Math.sqrt(3);
