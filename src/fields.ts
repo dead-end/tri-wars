d4d6f1c52f611cea9370db4468481b3170045a70
Dead End <dead.end@inbox.ru>
2026-08-21 23:42:08 +0200

    added todo

--- src/fields.ts
+++ src/fields.ts
@@ -1,5 +1,8 @@
 import { TField, TPoint } from './types';
 
+/**
+ * TODO: The field will contain objects (e.g. ships) or markers (highlight)
+ */
 export const fieldCreate = (hex: TPoint): TField => {
   return {
     hex: hex,
