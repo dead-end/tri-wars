77efffe07f78c7b30e393ebe370a9b2f50c0410b
Dead End <dead.end@inbox.ru>
2026-09-06 09:21:39 +0200

    update

--- src/scene/IObject.ts
+++ src/scene/IObject.ts
@@ -1,4 +1,6 @@
-// TODO: change the name
+/**
+ * The interface describes an object on a field of the board.
+ */
 export interface IObject {
   update(deltaTime: number): void;
   render(ctx: CanvasRenderingContext2D): void;
